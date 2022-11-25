2a4e3f8c6bfa5ee07905d964f26db39582590dcf
karl joyeux <karl.joyeux@efrei.net>
2022-11-25 21:08:01 +0100

    creating the base model for select components

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <label> {{ label }} </label>
+  <input 
+  v-bind="$attrs"
+  :placeholder="label"
+  class ="field"
+  :value = "modelValue"
+  @input="$emit('update:modelValue', $event.target.value)"
+  >
+  
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    label : {
+      type :String,
+      default :"",
+    }, 
+    modelValue: {
+      type : [Number, String],
+      default : "", 
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,27 @@
+<template>
+ <label v-if="label"> {{label}}  </label>
+ <select
+    class ="field"
+    :value ="modelValue">
+    <option 
+    v-for="option in categories"
+    :value="option"
+    :key ="option"
+    :selected="option===event.category">{{option}}</option>
+</select>
+</template>
+<script>
+    export default {
+        props : {
+            label :{
+                default : "",
+                type : String,
+            },
+            modelValue : {
+                default : "",
+                type : [Number, String]
+            }
+        }
+
+}
+</script>
